9450a471212194c5eff576266f36639c26b7cf4d
Sam Wang <samuelwangkc@gmail.com>
2019-03-07 20:51:35 -0800

    added inbox

--- public/js/inbox.js
+++ public/js/inbox.js
@@ -4,18 +4,19 @@
 //.. might carry over from init page load of socializing check with console log
 var uid = localStorage.getItem("currentUser");
 var hasInbox = false;
-var counter = 0;
 var username;
 var userPicture;
+var rUser;
+var rUserPicture;
 
 
 window.onload = function () {
     console.log(uid);
     database = firebase.database().ref(`1on1`);
     database.once("value").then(function (snapshot) {
-        if(snapshot.child(uid).exists()){
+        if (snapshot.child(uid).exists()) {
             hasInbox = true;
-            checkNumofThreads();
+            createThreads();
         }
     });
 
@@ -26,26 +27,34 @@ $("#test-ava").on("click", function (event) {
 
 });
 
-function checkNumofThreads(){
+function createThreads() {
     console.log(uid);
     console.log(hasInbox);
-    if(hasInbox){
+    if (hasInbox) {
         database = firebase.database().ref(`1on1/${uid}`);
         database.on("child_added", function (snapshot) {
-            counter++;
-            console.log("in loop"+counter);
-        });
-        console.log("out loop"+counter);
-        $.get("/api/users" + uid, function (data) {
-            // console.log("data Stored in mysql :", data.displayName);
-            username = data.displayName;
-            userPicture = data.picture;
-            $(".avator").attr("src", data.picture);
-            $("#displayName").html(data.displayName);
-            //   $('#actualName').append('<span id="add_here" style ="color: black;>' + data.actualName + '</span>');
-            $('#actualName').html(data.actualName);
+            console.log("tn" + snapshot.val().threadName);
+            console.log("chatwith" + snapshot.val().chatwith);
+            var chatwith = snapshot.val().chatwith;
+            console.log("VOID" + chatwith);
+            $.get("/api/users/" + chatwith, function (data) {
+                //console.log("sql data"+data);
+                rUser = data.displayName;
+                rUserPicture = data.picture;
+                console.log(`receiving: ${rUser} picture: ${rUserPicture}`);
+                drawIcons(rUserPicture,rUser);
+            });
         });
     }
+}
+
+function drawIcons(picture, userName) {
+    var template = `<div class ="valign center" style="border-radius: 50%;  height: 4em; width: 4em; display:inline-block; margin-right: .5em;">
+    <!-- Avatar -->
+    <img class="valign center" src="${picture}" alt="Avatar" style="width: 100%; transform: scale(1.1);">
+    <p class="valign center" style='margin-top: -.05em'>${userName}</p>
+    </div>`;
+    $(".inbox-bar").append(template);
 }
 //then write to the index bar
 //search through firebase to find in 1:1 to see if UID exists
